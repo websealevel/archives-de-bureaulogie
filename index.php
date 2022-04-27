c3b225a576d025443eb480e26ec1153ef58dfc12
paul <paul@genesis.websealevel.com*>
2022-04-27 07:15:19 +0200

    error et exception handler

--- index.php
+++ index.php
@@ -7,6 +7,20 @@
 
 require 'utils.php';
 
+//Global Exception handler
+set_exception_handler(function (Exception $e) {
+    echo 'Oups, il y a eu un problème :/' . PHP_EOL;
+    die;
+});
+//Global Error handler
+ini_set('error_log', 'error_log');
+set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+    echo 'Oups, il y a eu un problème :/' . PHP_EOL;
+    $error = sprintf("%s %s %s %s", $errno, $errstr, $errfile, $errline);
+    error_log($error);
+    die;
+});
+
 
 //Configurer la gestion des erreurs : tout doit aller dans un fichier log une fois en prod
 
@@ -23,3 +37,7 @@ if (!is_source_valid($file_source)) {
 echo 'Le fichier source ' . $file_source . ' est valide.';
 
 echo 'Génération des extraits' . PHP_EOL;
+
+
+
+restore_exception_handler();
