3562bd1ff4d285f07ae0d20606598762b354ca76
paul <paul@genesis.websealevel.com*>
2022-04-28 17:18:40 +0200

    fix init sql. 3 tables: accounts, roles et table de jointure

--- docker_postgres_init.sql
+++ docker_postgres_init.sql
@@ -1,6 +1,22 @@
-CREATE TABLE student (
-    id bigint NOT NULL,
-    name text COLLATE pg_catalog."default",
-    CONSTRAINT student_pkey PRIMARY KEY (id)
+CREATE TABLE accounts (
+    user_id serial PRIMARY KEY,
+    pseudo VARCHAR (50) UNIQUE NOT NULL,
+    password VARCHAR (50) NOT NULL,
+    email VARCHAR (255) UNIQUE NOT NULL,
+    created_on TIMESTAMP NOT NULL,
+    last_login TIMESTAMP
 );
 
+CREATE TABLE roles(
+    role_id serial PRIMARY KEY,
+    role_name VARCHAR (255) UNIQUE NOT NULL
+);
+
+CREATE TABLE account_roles (
+    user_id INT NOT NULL,
+    role_id INT NOT NULL,
+    grant_date TIMESTAMP,
+    PRIMARY KEY (user_id, role_id),
+    FOREIGN KEY (role_id) REFERENCES roles (role_id),
+    FOREIGN KEY (user_id) REFERENCES accounts (user_id)
+);
