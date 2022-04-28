58a0b57a04276090c29422ff7350217f740ab997
paul <paul@genesis.websealevel.com*>
2022-04-28 14:21:17 +0200

    dockerisation du projet sous forme web ok

--- web/index.php
+++ web/index.php
@@ -10,12 +10,4 @@ require_once 'src/header.php';
 
 echo 'ackboo out of context' . PHP_EOL;
 
-$download_request = new DownloadRequest(
-    'https://www.youtube.com/watch?v=ilhsfnSCQYE',
-    'Foo bar',
-    '1'
-);
-
-action_download_video($download_request);
-
 require_once 'src/footer.php';
