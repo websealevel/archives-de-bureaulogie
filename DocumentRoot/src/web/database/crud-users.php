67f0049da8491aea81261f404dfa2be13a298ef6
paul <paul@genesis.websealevel.com*>
2022-05-04 06:30:56 +0200

    wip refactor queries

--- DocumentRoot/src/web/database/crud-users.php
+++ DocumentRoot/src/web/database/crud-users.php
@@ -11,6 +11,8 @@ require_once __DIR__ . '/../../models/User.php';
 require_once __DIR__ . '/../../models/Notice.php';
 require_once __DIR__ . '/../database/connection.php';
 require_once __DIR__ . '/../router/router.php';
+require_once __DIR__ . '/queries_user.php';
+
 /**
  * Insère un utilisateur en base de données
  * @param User $user
@@ -18,35 +20,17 @@ require_once __DIR__ . '/../router/router.php';
  */
 function create_account(User $user): string|bool
 {
-    $db = connect_to_db();
-
-    $sql = 'INSERT INTO accounts(pseudo, password, email, created_on, has_reached_majority, has_accepted_the_chart, major, option, grade  )'
-        . 'VALUES(:pseudo,:password,:email,:created_on, :has_reached_majority, :has_accepted_the_chart, :major, :option, :grade )';
-
-    $stmt = $db->prepare($sql);
-
-    $stmt->bindValue(':pseudo', $user->pseudo);
-    $stmt->bindValue(':password', $user->password);
-    $stmt->bindValue(':email', $user->email);
-    $stmt->bindValue(':created_on', date('Y-m-d H:i:s'));
-    $stmt->bindValue(':has_reached_majority', true);
-    $stmt->bindValue(':has_accepted_the_chart', true);
-    $stmt->bindValue(':major', 'cable_managment');
-    $stmt->bindValue(':option', '');
-    $stmt->bindValue(':grade', 'studentL1');
-
     try {
-        $stmt->execute();
+        $id = sql_insert_account($user->pseudo, $user->password, $user->email);
     } catch (PDOException $e) {
         error_log($e);
         $_SESSION['notices'] = array(
             new Notice("Un membre de l'Université Libre de Bureaulogie porte déjà ce pseudonyme ou dispose déjà de cet email. Veuillez en essayer un autre s'il vous plaît", NoticeStatus::Error)
         );
-
         redirect('/sign-up');
     }
 
-    return $db->lastInsertId();
+    return $id;
 }
 
 /**
@@ -57,27 +41,20 @@ function create_account(User $user): string|bool
 function log_user(array $credentials)
 {
 
-    $pseudo = $credentials['login'];
-
-    //Select par pseudo
-    $db = connect_to_db();
-    $sql = 'SELECT pseudo, password FROM accounts where pseudo = :pseudo ';
-    $stmt = $db->prepare($sql);
+    $pseudo = $credentials['login'];;
 
     try {
-        $stmt->execute();
-        $result = $stmt->fetchObject();
-        dd($result);
+        $account = sql_find_account_by_pseudo($pseudo);
     } catch (PDOException $e) {
         error_log($e);
         $_SESSION['notices'] = array(
-            new Notice("Une erreur technique est survenue.", NoticeStatus::Error)
+            new Notice("Impossible de trouver votre compte, veuillez réessayer s'il vous plaît.", NoticeStatus::Error)
         );
         redirect('/');
     }
 
-    //Si pas trouvé, on rejette
     //Si trouvé, on check mdp, si pas ok, on rejette
+
     //Sinon on log
 
 
