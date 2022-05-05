2ac42e8e98e9f5802bf99b28897cf4bf08e866fa
paul <paul@genesis.websealevel.com*>
2022-05-05 07:57:05 +0200

    ajout roles et capabilities au schema, refactor ancien schema

--- docker_postgres_init.sql
+++ docker_postgres_init.sql
@@ -1,5 +1,17 @@
+CREATE TABLE roles(
+    role_id serial PRIMARY KEY,
+    role_name VARCHAR (255) UNIQUE NOT NULL
+);
+
+CREATE TABLE capabilities (
+    cap_id serial PRIMARY KEY,
+    cap VARCHAR (255) UNIQUE NOT NULL,
+    cap_label VARCHAR (255) NOT NULL
+);
+
 CREATE TABLE accounts (
     id serial PRIMARY KEY,
+    role_id INT NOT NULL,
     pseudo VARCHAR (50) UNIQUE NOT NULL,
     password VARCHAR (255) NOT NULL,
     email VARCHAR (255) UNIQUE NOT NULL,
@@ -7,19 +19,83 @@ CREATE TABLE accounts (
     last_login TIMESTAMP,
     has_reached_majority BOOLEAN NOT NULL,
     has_accepted_the_chart BOOLEAN NOT NULL,
-    heard_about_bureaulogy VARCHAR(255)
+    heard_about_bureaulogy VARCHAR(255),
+    FOREIGN KEY (role_id) REFERENCES roles (role_id)
 );
 
-CREATE TABLE roles(
-    role_id serial PRIMARY KEY,
-    role_name VARCHAR (255) UNIQUE NOT NULL
-);
-
-CREATE TABLE account_roles (
-    user_id INT NOT NULL,
+-- Table de jointure roles-capabilities.
+CREATE TABLE roles_capabilities (
     role_id INT NOT NULL,
-    grant_date TIMESTAMP,
-    PRIMARY KEY (user_id, role_id),
-    FOREIGN KEY (role_id) REFERENCES roles (role_id),
-    FOREIGN KEY (user_id) REFERENCES accounts (user_id)
+    cap_id INT NOT NULL,
+    PRIMARY KEY (role_id, cap_id),
+    FOREIGN KEY (role_id) REFERENCES roles (role_id)
 );
+
+-- Inserer les roles.
+INSERT INTO
+    roles(role_name)
+VALUES
+    ('superadmin'),
+    ('admin'),
+    ('moderateur'),
+    ('contributeur');
+
+-- Inserer les capabilites.
+INSERT INTO
+    capabilities(cap, cap_label)
+VALUES
+    ('add_admin', 'Ajouter un compte administrateur'),
+    ('add_moderator', 'Ajouter un compte modérateur'),
+    (
+        'edit_all_references',
+        'Éditer les références bibliographiques de tout le monde'
+    ),
+    (
+        'list_all_references',
+        'Lister les références bibliographiques de tout le monde'
+    ),
+    (
+        'list_all_clips',
+        'Lister les extraits vidéos de tout le monde'
+    ),
+    ('add_source', 'Ajouter une vidéo source'),
+    ('remove_source', 'Supprimer une vidéo source'),
+    ('remove_clip', 'Supprimer un clip'),
+    ('mod_references', 'Modérer les références'),
+    ('mod_clips', 'Modérer les extraits vidéos'),
+    ('submit_clip', 'Proposer un extrait vidéo'),
+    ('submit_reference', 'Proposer une référence'),
+    (
+        'list_my_references',
+        'Lister toutes ses références soumises (approuvées ou non'
+    ),
+    (
+        'list_my_clips',
+        'Lister tous ses extraits vidéos approuvés'
+    ),
+    (
+        'list_all_sources',
+        'Lister toutes les sources vidéos'
+    );
+
+-- - superadmin
+--   - tous les droits admin
+--   - ajouter un admin
+-- - admin 
+--   - tous les droits modérateur
+--   - ajouter un modérateur
+--   - éditer ressources bilbio de tout le monde
+--   - lister toutes les ressources biblio
+--   - lister tous les extraits
+--   - ajouter une source
+--   - supprimer une source
+--   - supprimer un extrait
+-- - modérateur
+--   - modérer une ressource biblio
+--   - modérer un extrait vidéo
+-- - contributeur
+--   - proposer un extrait vidéo
+--   - proposer une ressource biblio
+--   - voir ses extraits vidéos par source
+--   - voir ses ressources biblios
+--   - voir toutes les vidéos sources
