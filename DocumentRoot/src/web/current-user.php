bc867fa4a5483662d9eb03a84f80f951721bb8e7
paul <paul@genesis.websealevel.com*>
2022-05-07 16:26:45 +0200

    fix current_user_can si account_id est pas en session

--- DocumentRoot/src/web/current-user.php
+++ DocumentRoot/src/web/current-user.php
@@ -42,6 +42,9 @@ function current_user_can(string $cap): bool
     }
     $authentificated_user_id = from_session('account_id');
 
+    if (empty($authentificated_user_id))
+        return false;
+
     $authentificated_user_role = sql_find_role_of_account($authentificated_user_id);
 
     //Check si le role a la cap
