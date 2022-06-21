16b6bbeba782b6648fb78db387cb6f6cc161ba63
websealevel <contact@websealevel.com>
2022-06-21 11:33:31 +0200

    cleaning

--- DocumentRoot/src/web/output-html.php
+++ DocumentRoot/src/web/output-html.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Contient toutes les fonctions de sortie HTML
+ * Contient toutes les fonctions de sortie HTML échapées
  *
  * @package wsl 
  */
@@ -189,4 +189,68 @@ function enqueue_js_scripts(array $scripts = array())
     }
     echo '</script>';
     return;
+}
+
+/**
+ * Ecrit et échappe sur la sortie standard une valeur enregistrée dans la session
+ * @param string $key La clef demandée
+ * @param string $array_key Optional Default = ''. Si la valeur demandée est un tableau, renvoie la valeur sous la clef $array_key
+ * @return void
+ */
+function esc_html_from_session_e(string $key, string $array_key): void
+{
+    $session_value = from_session($key, $array_key);
+    esc_html_e($session_value);
+}
+
+/**
+ * Ecrit sur la sortie standard le message d'erreur associé à l'input du form s'il existe, rien sinon
+ * @param string $input_name Le nom du champ du formuliare
+ * @param InputValidation[] $form_errors
+ * @throws Exception - Si aucune session n'est ouverte
+ * @return void
+ */
+function esc_html_form_error_msg_e(string $input_name, string $key_form_errors)
+{
+
+    if (!isset($_SESSION) || !isset($_SESSION["{$key_form_errors}"]))
+        return;
+
+    $form_errors = $_SESSION["{$key_form_errors}"];
+
+    if (!array_key_exists($input_name, $form_errors))
+        return;
+
+    echo '<div class="error-message">';
+    esc_html_e($form_errors["{$input_name}"]->message);
+    echo '</div>';
+    return;
+}
+
+/**
+ * Ecrit sur la sortie standard les notices présentes en session
+ * @global $_SESSION
+ * @throws Exception - Si aucune session n'est ouverte
+ */
+function esc_html_notices_e()
+{
+    if (!isset($_SESSION) || !isset($_SESSION['notices']))
+        return;
+
+    $notices = $_SESSION['notices'];
+
+    if (!is_array($notices))
+        throw new Exception("@esc_html_notices_e: les notices ne sont pas empilées dans un tableau");
+
+    $html_notices = array_map(function (Notice $notice) {
+        return '<li class="notice ' . $notice->status->value . '">' . $notice->message . '</li>';
+    }, $notices);
+
+    echo '<ul class="notices">';
+    foreach ($html_notices as $html_notice) {
+        echo $html_notice;
+    }
+    echo '</ul>';
+
+    unset($_SESSION['notices']);
 }
