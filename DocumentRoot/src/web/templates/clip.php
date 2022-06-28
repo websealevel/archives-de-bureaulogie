c38a2c362eca4c384fd8fd26eb5434833fa9fd1e
websealevel <contact@websealevel.com>
2022-06-28 09:34:13 +0200

    template part pour contols clip - suppresion controles sur la prévisualisation (confusant)

--- DocumentRoot/src/web/templates/clip.php
+++ DocumentRoot/src/web/templates/clip.php
@@ -52,21 +52,17 @@ $token = register_api_token($account, 'submit_clip');
             </div>
         </fieldset>
 
-
-
         <fieldset id="fieldset-video-clip" width="100%">
             <legend>
                 <h3>Prévisualisation</h3>
             </legend>
             <div class="video-clip">
-                <video id="video-clip" width="100%" controls>
+                <video id="video-clip" width="100%">
                     <source src="" type="video/mp4">
                     Votre navigateur ne supporte pas le tag video HTML5 :(
                 </video>
             </div>
         </fieldset>
-
-
     </div>
 
     <div class="errors" style="color: red;"></div>
@@ -75,44 +71,7 @@ $token = register_api_token($account, 'submit_clip');
 
         <small>Les champs marqués d'un asterisque(*) sont obligatoires</small>
 
-        <div>
-
-        </div>
-        <fieldset id="fieldset-controls">
-            <legend>Contrôles</legend>
-            <input name=" btn_rewind_5_s" id="btn_rewind_5_s" class="btn-controls rewind" type="button" title="Reculer de 5s" value="<<" title="reculer de 5s">
-
-            <input name=" btn_rewind_1_s" id="btn_rewind_1_s" class="btn-controls rewind" type="button" value="<" title="Reculer de 1s (Raccourci : flèche gauche)">
-
-            <input name=" btn_forward_1_s" id="btn_forward_1_s" type="button" title="Avancer de 1s" value=">" class="btn-controls forward" title="avancer de 1s (flèche droite)">
-            <input name=" btn_forward_5_s" id="btn_forward_5_s" type="button" title="Avancer de 5s" value=">>" class="btn-controls forward" title="avancer de 5s">
-
-            <div id="clip-duration">
-                0
-            </div>
-
-            <div class="label-input">
-                <label for="timecode_start">Début*</label>
-                <input type="text" name="timecode_start" id="timecode_start" pattern="[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3}" value="00:00:00.000" title="Veuillez renseigner un timecode au format hh:mm:ss.lll. 
-Sinon, utiliser le bouton 'Démarrer l'extrait ici' prévu à cet effet">
-            </div>
-
-            <div class="label-input">
-                <label for="timecode_end">Fin*</label>
-                <input type="text" name="timecode_end" id="timecode_end" value="00:00:12.000" pattern="[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3}" title="Veuillez renseigner un timecode au format hh:mm:ss.lll
-Sinon, utiliser le bouton 'Finir l'extrait ici' prévu à cet effet">
-            </div>
-
-            <div id="edition-actions">
-                <input name=" btn_clip_start" id="btn_clip_start" type="button" class="btn-edition" title="Définir la position courante du curseur de lecture comme timecode de départ de l'extrait" value="Démarrer l'extrait ici">
-
-                <input name="bt_clip_end" id="btn_clip_end" type="button" class="btn-edition" title="Définir la position courante du curseur de lecture comme timecode de fin de l'extrait" value="Finir l'extrait ici">
-
-                <input type="button" id="btn_preview" value="Prévisualiser" class="btn-edition">
-
-                <input type="button" id="btn_preview_tail" value="Prévisualiser la traîne" class="btn-edition" title="La traine correspond à la portion située juste après le timecode de fin, afin de mieux visualiser la fin du cut">
-            </div>
-        </fieldset>
+        <?php require_once 'parts/clip-controls.php' ?>
 
         <fieldset id="fieldset-edition">
 
