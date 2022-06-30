f4bc83d98e29f0af9cb707d0d687b6858389f5d4
websealevel <contact@websealevel.com>
2022-06-30 10:07:03 +0200

    wip nous soutenir

--- DocumentRoot/src/web/templates/nous-soutenir.php
+++ DocumentRoot/src/web/templates/nous-soutenir.php
@@ -14,12 +14,21 @@ present_header();
 
 <h2>Nous soutenir</h2>
 
-
 <p>Page en construction...</p>
 
 <h3>Soutenir Canard PC</h3>
+
+Si vous souhaitez nous soutenir, commencez plutôt par <a href="https://boutique.canardpc.com/common/product-subscription/4">soutenir Canard PC</a>, d'autant plus que le canard perd ses plumes en ce moment.
+
 <h3>Soutenir ackboo</h3>
+
+Si vous souhaitez toujours nous soutenir après avoir pris votre abonnement à Canard PC, alors soutenez d'abord ackboo, père fondateur de la bureaulogie. Sans lui, les archives n'auraient pas de raison d'être.
+
 <h3>Soutenir les archives</h3>
+
+S'il vous reste encore quelques centimes dans les poches après avoir soutenu Canard PC et ackboo, vous pouvez nous soutenir modestement pour nous aider à couvrir les frais d'hébergement du site. Cela sera possible quand les archives seront ouvertes au public.
+
+Merci pour votre soutien à Canard PC, ackboo et aux archives.
 
 <small>Les archives de bureaulogie</small>
 
