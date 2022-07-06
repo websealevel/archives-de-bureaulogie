aa17ac37be3c626a7eac42b347037dc89e21aad6
websealevel <contact@websealevel.com>
2022-07-06 11:45:14 +0200

    wip: memcacheD configuration

--- DocumentRoot/index.php
+++ DocumentRoot/index.php
@@ -12,6 +12,11 @@ require_once './src/models/Notice.php';
 require_once './src/web/router/router.php';
 require_once './src/handlers.php';
 
+
+
+phpinfo();
+// exit;
+
 load_env();
 
 if (in_maintenance_mode()) {
--- DocumentRoot/src/web/templates/home.php
+++ DocumentRoot/src/web/templates/home.php
@@ -14,10 +14,7 @@ require_once __DIR__ . '/../utils.php';
 require_once __DIR__ . '/../session.php';
 require_once __DIR__ . '/../current-user.php';
 require_once __DIR__ . '/../../handlers.php';
-
 ?>
-
-<?php phpinfo(); ?>
 
 <?php present_header(); ?>
 
